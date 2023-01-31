64d71cb905c495ad0436ca0b390f2a7e7a58f5d2
Bjoern Mayer <u301101@levante3.lvt.dkrz.de>
2023-01-31 17:01:17 +0100

    Added basin file

--- config.py
+++ config.py
@@ -16,8 +16,11 @@ data_path = os.path.join(proj_path, "data")
 data_raw_path = os.path.join(data_path, "raw")
 data_pro_path = os.path.join(data_path, "processed")
 data_res_path = os.path.join(data_path, "results")
+data_ext_path = os.path.join(data_path, "external")
 
 src_path = os.path.join(proj_path, "src")
 
 
 
+# Basin File
+basin_file = os.path.join(data_ext_path, "GR15_basin.nc")
--- src/00_prepare_data/link_raw_data.sh
+++ src/00_prepare_data/link_raw_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-
 mkdir -p /work/uo1075/u301101/Doktorarbeit/CNN/data/raw/GrandEnsemble
 cp -rsv /work/uo1075/u301101/Doktorarbeit/Data/GrandEnsemble/hist /work/uo1075/u301101/Doktorarbeit/CNN/data/raw/GrandEnsemble
+
+ln -s /pool/data/MPIOM/GR15/GR15_basin.nc /work/uo1075/u301101/Doktorarbeit/CNN/data/external/
